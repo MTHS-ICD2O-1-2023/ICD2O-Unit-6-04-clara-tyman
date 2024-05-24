66920ca0a9f631b376e1f5fd7530c647912d8018
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-24 18:41:04 +0000

    all code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get('radius')
+  console.log(radius)
+
+  // process
+  const volume = 4 / 3 * Math.PI * Math.pow(radius, 3)
+  const dimensions =
+    "<ul>\n<li>radius = " +
+    radius
+
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+}
